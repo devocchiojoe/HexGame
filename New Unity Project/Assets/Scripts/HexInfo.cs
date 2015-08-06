052cce280eb9f10d49b433c1f416a68654a85bdb
Joe Ocean <josephocchionero@Josephs-MacBook-Pro.local>
2015-08-05 21:40:37 -0400

    initial commit for project

---
+++ New Unity Project/Assets/Scripts/HexInfo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexInfo : MonoBehaviour 
+{
+    // basic hexgon mesh making
+    public Vector3[] vertices;
+    public Vector2[] uv;
+    public int[] triangles;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    void MeshSetup() {
+        // region verts
+        floor_level = 0;
+        vertices = new Vector3[] { // has to be clockwise
+            new Vector3 (-1f, floor_level, -0.5f),
+            new Vector3 (-1f, floor_level, 0.5f),
+            new Vector3 (0f, floor_level, 1f),
+            new Vector3 (1f, floor_level, 0.5f),
+            new Vector3 (1f, floor_level, -0.5f),
+            new Vector3 (0f, floor_level, -1f)
+		};
+        // endregion
+        
+        // ragion triangles
+        // endregion
+        
+        // region uv
+        // endregion
+        
+        // region finalize
+        // endregion
+    } 
+
+
+    // Update is called once per frame
+    //    void Update () {
+    //}
+}
